ce73c69960f1baff678cdeb211df7415ce189579
Kamil Mrzygłód <kamz@demant-technology.com>
2018-06-25 09:31:09 +0200

    Added chapter 03

---
+++ ch3/AksStart/create-service-principal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ACR_NAME=myacrinstance
+SERVICE_PRINCIPAL_NAME=acr-service-principal
+
+# Populate the ACR login server and resource id.
+ACR_LOGIN_SERVER=$(az acr show --name $ACR_NAME --query loginServer --output tsv)
+ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
+
+# Create a contributor role assignment with a scope of the ACR resource.
+SP_PASSWD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --role Reader --scopes $ACR_REGISTRY_ID --query password --output tsv)
+
+# Get the service principle client id.
+CLIENT_ID=$(az ad sp show --id http://$SERVICE_PRINCIPAL_NAME --query appId --output tsv)
+
+# Output used when creating Kubernetes secret.
+echo "Service principal ID: $CLIENT_ID"
+echo "Service principal password: $SP_PASSWD"
